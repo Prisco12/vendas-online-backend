ebffa4f1b438d2e5c2c3932e8a278c05978635a5
Gabriel Prisco <gabrielmarcosprisco@gmail.com>
2025-09-18 22:52:52 -0300

    feat: criando service do user e get all users

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,18 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import type CreateUserDto from './dtos/createUser.dto';
+import { UserService } from './user.service';
+import { User } from './interfaces/user.interface';
 
 @Controller('user')
 export class UserController {
+    constructor( private readonly userService: UserService ) {} 
     @Post()
-    async createUser( @Body() createUserDto: CreateUserDto ) {
-        return createUserDto;
+    async createUser( @Body() createUserDto: CreateUserDto ): Promise<User> {
+        return this.userService.createUser(createUserDto);
+    }
+
+    @Get()
+    async getAllUsers(): Promise<User[]> { 
+        return this.userService.getAllUsers();
     }
 }
