1060c1bd3906e38cdc1957a8fc4d167760fbbf60
Gabriel Prisco <gabrielmarcosprisco@gmail.com>
2025-09-20 11:30:08 -0300

    feat: create module auth

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,22 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { UserEntity } from 'src/user/entities/user.entity';
+import { LoginDto } from './dtos/login.dto';
+import { UserService } from 'src/user/user.service';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class AuthService {
+    constructor(
+        private readonly userService: UserService,
+    ) {}
+
+    async login(loginDto: LoginDto): Promise<UserEntity> {
+        const user: UserEntity | undefined = await this.userService.findUserByEmail(loginDto.email).catch(() => undefined);
+        
+        if (!user || !(await bcrypt.compare(loginDto.password, user.password))) {
+           throw new NotFoundException('Email or Password invalid');
+        }
+
+        return user;
+    }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -56,4 +56,18 @@ export class UserService {
 
         return user;
     }
+
+    async findUserByEmail(email: string): Promise<UserEntity> {
+        const user = await this.userRepository.findOne({
+            where: {
+                email: email,
+            }
+        })
+
+        if (!user) {
+            throw new NotFoundException(`User with email ${email} not found.`);
+        }
+
+        return user;
+    }
 }
