ca1ef8ef9c1c227bdabecea97f8b60b74776fe89
Gabriel Prisco <gabrielmarcosprisco@gmail.com>
2025-09-20 10:36:08 -0300

    feat: insert relation many-to-one

--- src/address/entities/address.entity.ts
+++ src/address/entities/address.entity.ts
@@ -1,4 +1,6 @@
-import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { CityEntity } from "src/city/entities/city.entity";
+import { UserEntity } from "src/user/entities/user.entity";
+import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity({ name: 'address' })
 export class AddressEntity {
@@ -26,5 +28,11 @@ export class AddressEntity {
     @CreateDateColumn({name: 'updated_at'})
     updatedAt: Date;
 
+    @ManyToOne(() => UserEntity, (user) => user.addresses)
+    @JoinColumn({ name: 'user_id', referencedColumnName: 'id' })
+    user?: UserEntity;
 
+    @ManyToOne(() => CityEntity, (city) => city.addresses)
+    @JoinColumn({ name: 'city_id', referencedColumnName: 'id' })
+    city?: CityEntity;
 }
--- src/city/entities/city.entity.ts
+++ src/city/entities/city.entity.ts
@@ -1,4 +1,6 @@
-import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { AddressEntity } from "src/address/entities/address.entity";
+import { StateEntity } from "src/state/entities/state.entity";
+import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity({ name: 'city' })
 export class CityEntity {
@@ -17,5 +19,10 @@ export class CityEntity {
     @CreateDateColumn({name: 'updated_at'})
     updatedAt: Date;
 
+    @OneToMany(() => AddressEntity, (address) => address.city)
+    addresses?: AddressEntity[];
 
+    @ManyToOne(() => StateEntity, (state) => state.cities)
+    @JoinColumn({ name: 'state_id', referencedColumnName: 'id' })
+    state?: StateEntity;
 }
